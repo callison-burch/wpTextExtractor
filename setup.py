41fd50ee79ef5254f42dbfc19fd6a74e2228e7c1
bahn <byunggyu.ahn@gmail.com>
2010-09-28 16:40:12 -0400

    wpTextExtractor modularized

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='wpTextExtractor',
+      version='0.1.0',
+      description='Python Client Library to extract plain text from wikipedia articles',
+      author='Chris Callison-Burch',
+      author_email='ccb@cs.jhu.edu',
+      url='http://github.com/callison-burch/wpTextExtractor',
+      packages=['wpTextExtractor'])
